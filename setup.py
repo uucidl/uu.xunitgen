bb34caea6110d24865df6dfae9ffed7883190e1f
Nicolas Léveillé <nicolas@uucidl.com>
2014-05-26 22:17:49 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name="xunitgen",
+    version="0.1.6",
+    description="Generate xUnit.xml files",
+    author="Nicolas Léveillé",
+    author_email="nicolas@uucidl.com",
+    url="https://github.com/uucidl/uu.xunitgen",
+    packages=["xunitgen"],
+)
