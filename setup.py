d9e4df8a5d24e554e510a90fb6fc46f0f7000392
nicolas <nicolas@uucidl.com>
2017-12-10 13:53:22 +0100

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name="xunitgen",
-    version="1.0.0",
+    version="1.0.1",
     description="Generate xUnit.xml files",
     author="Nicolas Léveillé",
     author_email="nicolas@uucidl.com",
     url="https://github.com/uucidl/uu.xunitgen",
     packages=["xunitgen"],
     license='MIT',
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
 )
